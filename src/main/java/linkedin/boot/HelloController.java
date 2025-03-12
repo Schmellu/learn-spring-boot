044b413fa100cb5d6d1486581ec9f41eb8f06365
Schmellu <minabean82@gmail.com>
2025-03-12 14:06:47 +0000

    Erstes Projekt

---
+++ src/main/java/linkedin/boot/HelloController.java
@@ -0,0 +1,13 @@
+package linkedin.boot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+  @GetMapping("/")
+  public String get() {
+    return "Hello, currently testing";
+  }
+}
